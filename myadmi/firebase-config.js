914a9fc24eacd2674749e8354551ce70049f0874
Mkmaayanaa <mohkabeer0@gmail.com>
2025-12-05 06:14:47 +0100

    Create firebase-config.js

---
+++ myadmi/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuMt3PtZBVvowQ-bT0sa6CsWKYKqd5xs8",
+  authDomain: "klf-admin.firebaseapp.com",
+  projectId: "klf-admin",
+  storageBucket: "klf-admin.firebasestorage.app",
+  messagingSenderId: "111325814314",
+  appId: "1:111325814314:web:a20e2dfee2d58d975eed53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
